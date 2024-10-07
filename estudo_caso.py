17994157fef758f1bd239f18cbbf71b435cd240f
davidfontess <87153638+davidfontess@users.noreply.github.com>
2024-10-07 14:12:17 -0300

    Update estudo_caso.py
    adição de geração das tabelas da equação

--- estudo_caso.py
+++ estudo_caso.py
@@ -1,22 +1,34 @@
 from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 
-#Definicao da estrutura do modelo. Vinculos 
-model = BayesianNetwork([('H', 'D'), ('D', 'S'), ('D', 'E')])
+# define da estrutura do modelo com as novas relações
+model = BayesianNetwork([('H', 'D'), ('H', 'E'), ('D', 'S'), ('E', 'S')])
 
-#Criação das tabelas de probabilidade condicional
-#variable_card define o número de valores possíveis que a variável pode assumir
-#evidence_card faz a mesma coisa mas para a evidência (a quem a variavel depende)
-
+# cria das tabelas de probabilidade condicional (CPDs)
+# tabela de probabilidade pra H
 cpd_h = TabularCPD(variable='H', variable_card=2, values=[[0.2], [0.8]])
 
-cpd_d = TabularCPD(variable='D', variable_card=2, values=[[0.1, 0.01],[0.9, 0.99]], evidence=['H'], evidence_card=[2])
+# tabela de probabilidade condicional D dado H
+cpd_d = TabularCPD(variable='D', variable_card=2, values=[[0.1, 0.01], [0.9, 0.99]],
+                   evidence=['H'], evidence_card=[2])
 
-cpd_s = TabularCPD(variable='S', variable_card=2, values=[[0.8, 0.1],[0.2, 0.9]], evidence=['D'], evidence_card=[2])
+# tabela de probabilidade condicional E dado H
+cpd_e = TabularCPD(variable='E', variable_card=2, values=[[0.9, 0.05], [0.1, 0.95]],
+                   evidence=['H'], evidence_card=[2])
 
-cpd_e = TabularCPD(variable='E', variable_card=2, values=[[0.9, 0.05],[0.1, 0.95]], evidence=['D'], evidence_card=[2])
+# tabela de probabilidade condicional S dado D e E
+cpd_s = TabularCPD(variable='S', variable_card=2, values=[[0.8, 0.1, 0.2, 0.05],
+                                                          [0.2, 0.9, 0.8, 0.95]],
+                   evidence=['D', 'E'], evidence_card=[2, 2])
 
-#Associando CPD ao modelo
-model.add_cpds(cpd_h, cpd_d, cpd_s, cpd_e)
+# associa as CPDs ao modelo
+model.add_cpds(cpd_h, cpd_d, cpd_e, cpd_s)
 
-print(model.get_cpds('S'))
+# verifica se todas as CPDs estão associadas certo
+print("Modelo é válido: ", model.check_model())
+
+# exibi tabelas geradas
+print("\nTabela de Probabilidade Condicional para H:\n", cpd_h)
+print("\nTabela de Probabilidade Condicional para D dado H:\n", cpd_d)
+print("\nTabela de Probabilidade Condicional para E dado H:\n", cpd_e)
+print("\nTabela de Probabilidade Condicional para S dado D e E:\n", cpd_s)
